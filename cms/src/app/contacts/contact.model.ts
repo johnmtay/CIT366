2d2f26b045bf4e72dec11b9a320b6c171778c3cb
johnmtay <tay18008@byui.edu>
2019-05-05 18:32:54 -0600

    Updated W02 CMS assignment

--- cms/src/app/contacts/contact.model.ts
+++ cms/src/app/contacts/contact.model.ts
@@ -1,9 +1,17 @@
 export class Contact {
+    public contactId: number;
     public name: string;
-    public imagePath: string;
+    public email: string;
+    public phone: number;
+    public imageUrl: string;
+    public group: string;
 
-    constructor(name: string, imagePath: string) {
+    constructor(contactId: number, name: string, email: string, phone: number, imageUrl: string, group: string) {
+        this.contactId = contactId;
         this.name = name;
-        this.imagePath = imagePath;
+        this.email = email;
+        this.phone = phone;
+        this.imageUrl = imageUrl;
+        this.group = group;
     }
 }
--- cms/src/app/contacts/contacts-detail/contacts-detail.component.ts
+++ cms/src/app/contacts/contacts-detail/contacts-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Contact } from '../contact.model';
+
 @Component({
   selector: 'cms-contacts-detail',
   templateUrl: './contacts-detail.component.html',
